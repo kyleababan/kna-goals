c989cc3be58828dd43c560f41607617e9c25dd2a
kyleababan <marionababan60@gmail.com>
2025-08-27 09:05:32 +0700

    Add firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-F8-RVdWsocJNLvBLqkKqOgZvD3O43RA",
+  authDomain: "kna-goals-tracker.firebaseapp.com",
+  projectId: "kna-goals-tracker",
+  storageBucket: "kna-goals-tracker.firebasestorage.app",
+  messagingSenderId: "728003501254",
+  appId: "1:728003501254:web:faf4451e6b9b71d258cbc7"
+};
+
+
+const app = initializeApp(firebaseConfig);
